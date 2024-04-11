fc1faf281e7c48b9fa5402b38ba0e667f81aaca0
Gokulkrishna-Raju <gmkrishna11082003@gmail.com>
2024-04-11 23:46:11 +0530

    Boiler Plate Code

---
+++ README.md
@@ -0,0 +1,13 @@
+# kpr_todo_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+primary - #624DE6
+background - #151726
+stroke - #323658
+subtitle - #9395D3
+completed - #8B8787
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("ToDo App"),
+        ),
+        body: const Center(
+          child: CenterButton()
+          ),
+      ),
+    );
+  }
+}
+
+class CenterButton extends StatelessWidget {
+  const CenterButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => Dialog(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const SizedBox(height: 15),
+                    const Text('Panniyae Aagurom '),
+                    const SizedBox(height: 15),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          child: const Text('poda venna 💪'),
+        ),
+      ],
+    );
+  }
+}
